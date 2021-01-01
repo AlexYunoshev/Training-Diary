f6f68778f64040b259ca047fcacc13620d4a1180
Administrator <yunoshev.alex@gmail.com>
2021-01-01 20:20:40 +0200

    Добавлен фон и базовое меню сверху

--- Training Diary/MainWindow.xaml.cs
+++ Training Diary/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Training_Diary
 {
     /// <summary>
@@ -23,6 +24,33 @@ namespace Training_Diary
         public MainWindow()
         {
             InitializeComponent();
+           
+        }
+
+        private void Main_Click(object sender, RoutedEventArgs e)
+        {
+
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursorMenu.Margin = new Thickness(0 + index * 170, -5, 0, 0);
+            //Main.Content = new Calendar();
+        }
+
+        private void Statistic_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursorMenu.Margin = new Thickness(0 + index * 170, -5, 0, 0);
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursorMenu.Margin = new Thickness(0 + index * 170, -5, 0, 0);
+        }
+
+        private void Developers_Click(object sender, RoutedEventArgs e)
+        {
+            int index = int.Parse(((Button)e.Source).Uid);
+            GridCursorMenu.Margin = new Thickness(0 + index * 170, -5, 0, 0);
         }
     }
 }
